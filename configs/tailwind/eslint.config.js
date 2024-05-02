d0ddbe176c057f7a7761823b6056cee4190380a7
Shahzad Qureshi <30411645+ShahzadQ@users.noreply.github.com>
2024-05-02 12:24:57 +0100

    chore: remove unneeded files and update project names

---
+++ configs/tailwind/eslint.config.js
@@ -0,0 +1,6 @@
+// FIXME: This kinda stinks...
+/// <reference types="../../configs/eslint/types.d.ts" />
+
+import baseConfig from "@workspace/eslint-config/base";
+
+export default [...baseConfig];
