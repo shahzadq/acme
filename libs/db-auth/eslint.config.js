2bfba0180971eb1ab90550d80447c263a8d264f4
Shahzad Qureshi <30411645+ShahzadQ@users.noreply.github.com>
2024-05-16 14:15:48 +0100

    chore: fix eslint configs removing random ignore path

--- libs/db-auth/eslint.config.js
+++ libs/db-auth/eslint.config.js
@@ -3,7 +3,7 @@ import baseConfig from "@workspace/eslint-config/base";
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
-    ignores: ["dist/**"],
+    ignores: [],
   },
   ...baseConfig,
 ];
