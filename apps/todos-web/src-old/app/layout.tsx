b12cfd9bd2350e5885d094b55d91ab7a059ad1ad
Shahzad Qureshi <30411645+ShahzadQ@users.noreply.github.com>
2024-06-11 18:43:09 +0100

    chore(apps/stodos-web): remove src-old

--- apps/todos-web/src-old/app/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { RootLayout } from "@workspace/web-ui/components/Layout/RootLayout";
-
-export default function Layout(props: { children: React.ReactNode }) {
-  return <RootLayout classNames={{ main: "flex-row" }} {...props} />;
-}
--- apps/todos-web/src-old/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { db } from "@workspace/db-todos";
-
-import { Todos } from "@/components/Todos";
-
-export default async function HomePage() {
-  const todos = await db.query.todoTable.findMany({ with: { list: true } });
-
-  return <Todos title="All" todos={todos} />;
-}
